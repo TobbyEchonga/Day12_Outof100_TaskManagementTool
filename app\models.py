6e6bfbef2379ce8eb2f9b4737412860d3f3387fd
Tobby Echonga <30405173+TobbyEchonga@users.noreply.github.com>
2024-01-18 18:34:13 +0300

    Adding app\models.py

---
+++ app\models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    due_date = db.Column(db.Date, nullable=True)
+    priority = db.Column(db.String(10), nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
